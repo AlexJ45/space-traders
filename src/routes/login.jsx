05942bbab6bd50b53dab4bb05a302c1b799fb97a
AlexJ <alexandre.j45000@gmail.com>
2024-01-11 17:36:19 +0100

    Map update

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,18 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import "/public/assets/css/login.css";
 
 const Login = () => {
   const [token, setToken] = useState("");
+  const [callSign, setCallSign] = useState("");
+  const [isRegisterView, setRegisterView] = useState(false);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const authToken = Cookies.get("authToken");
+    if (authToken) {
+      authenticateAndNavigate(authToken);
+    }
+  }, []);
+
+  const authenticateAndNavigate = async (token) => {
+    try {
+      const playerSymbol = await authenticateUserAndGetSymbol(token);
+      if (playerSymbol) {
+        Cookies.set("authToken", token, { expires: 7 });
+        navigate(`/passerelle/${playerSymbol}`);
+      } else {
+        console.error("Player is undefined");
+      }
+    } catch (error) {
+      console.error("Authentication error:", error);
+
+      if (error.message.includes("Unauthorized")) {
+        console.error(
+          "Unauthorized access. Redirect to login or handle accordingly."
+        );
+      }
+    }
+  };
 
   const handleTokenInput = (event) => {
     setToken(event.target.value);
   };
 
-  const handleLogin = async () => {
+  const handleCallSignInput = (event) => {
+    setCallSign(event.target.value);
+  };
+
+  const handleLogin = () => {
     try {
-      const playerSymbol = await authenticateUserAndGetSymbol(token);
+      const playerSymbol = authenticateUserAndGetSymbol(token);
       if (playerSymbol) {
         Cookies.set("authToken", token, { expires: 7 });
         navigate(`/passerelle/${playerSymbol}`);
@@ -21,39 +55,146 @@ const Login = () => {
       }
     } catch (error) {
       console.error("Authentication error:", error);
+      console.error(
+        "Unauthorized access. Redirect to login or handle accordingly."
+      );
+    }
+  };
+
+  const handleRegister = async () => {
+    try {
+      const response = await registerNewAgent(callSign);
+
+      if (response.status === 201) {
+        console.log("Agent registered successfully");
+        const registrationData = await response.json();
+        const registrationToken = registrationData.data.token;
+        setToken(registrationToken);
+        authenticateAndNavigate(registrationToken);
+      } else {
+        console.error("Agent registration failed:", response.statusText);
+
+        if (response.status === 401) {
+          console.error(
+            "Unauthorized access. Redirect to login or handle accordingly."
+          );
+        } else if (response.status === 400) {
+          console.error("Bad request. Handle accordingly.");
+        }
+
+        console.error("Error response:", response);
+      }
+    } catch (error) {
+      console.error("Agent registration failed:", error);
+
+      const errorText = error.response
+        ? await error.response.text()
+        : error.message;
+      console.error("Error response:", errorText);
+
+      throw new Error("Agent registration failed");
     }
   };
 
   const authenticateUserAndGetSymbol = async (inputToken) => {
     try {
       const response = await fetch("https://api.spacetraders.io/v2/my/agent", {
-        method: "GET",
         headers: {
-          "Content-Type": "application/json",
           Authorization: `Bearer ${inputToken}`,
         },
+        timeout: 1000,
       });
 
-      if (!response.ok) {
+      if (response.status >= 200 && response.status < 300) {
+        const responseData = await response.json();
+        return responseData.data.symbol;
+      } else {
+        console.error("Authentication failed. Response:", response);
         throw new Error(`Authentication failed: ${response.statusText}`);
       }
-
-      const responseData = await response.json();
-      return responseData.data.symbol;
     } catch (error) {
       console.error("Error during authentication:", error);
+      console.error("Error details:", error.response || error.message || error);
       throw new Error("Authentication failed");
     }
   };
 
+  const registerNewAgent = async (inputCallSign) => {
+    try {
+      const response = await fetch("https://api.spacetraders.io/v2/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          symbol: inputCallSign,
+          faction: "COSMIC",
+        }),
+      });
+
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      console.error("Error during agent registration:", error);
+      throw new Error("Agent registration failed");
+    }
+  };
+
+  const switchToRegisterView = () => {
+    setRegisterView(true);
+  };
+
+  const switchToLoginView = () => {
+    setRegisterView(false);
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-      <label>
-        Token:
-        <input type="text" value={token} onChange={handleTokenInput} />
-      </label>
-      <button onClick={handleLogin}>Login</button>
+    <div className="login">
+      <div id="switch" className="switch">
+        <div
+          className={`switch-choix ${
+            !isRegisterView ? "switch-actif" : ""
+          } button`}
+          onClick={switchToLoginView}
+        >
+          Login
+        </div>
+        <div
+          className={`switch-choix ${
+            isRegisterView ? "switch-actif" : ""
+          } button`}
+          onClick={switchToRegisterView}
+        >
+          Register
+        </div>
+      </div>
+
+      {isRegisterView ? (
+        <form className="register-form">
+          <label>
+            Call Sign:
+            <input
+              type="text"
+              value={callSign}
+              onChange={handleCallSignInput}
+            />
+          </label>
+          <button className="button" onClick={handleRegister}>
+            Register
+          </button>
+        </form>
+      ) : (
+        <form className="login-form">
+          <label>
+            Token:
+            <input type="text" value={token} onChange={handleTokenInput} />
+          </label>
+
+          <button className="button" onClick={handleLogin}>
+            Login
+          </button>
+        </form>
+      )}
     </div>
   );
 };
--- src/routes/passerelle.jsx
+++ src/routes/passerelle.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const Passerelle = ({ children }) => {
   const { symbol } = useParams();
   const [agentData, setAgentData] = useState(null);
+  const [isTokenCopied, setTokenCopied] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAgentData = async () => {
@@ -18,28 +20,57 @@ const Passerelle = ({ children }) => {
             },
           }
         );
-        const data = await response.json();
-        setAgentData(data.data);
+
+        if (response.ok) {
+          const responseData = await response.json();
+          setAgentData(responseData.data);
+
+          // Move the comparison inside the try block
+          if (symbol !== responseData.data.symbol) {
+            navigate(`/passerelle/${responseData.data.symbol}`);
+          }
+        } else {
+          console.error("Error fetching agent data:", response.statusText);
+        }
       } catch (error) {
         console.error("Error fetching agent data:", error);
       }
     };
 
     fetchAgentData();
-  }, []);
+  }, [symbol, navigate]);
+
+  const handleCopyToken = () => {
+    const authToken = Cookies.get("authToken");
+    navigator.clipboard.writeText(authToken);
+    setTokenCopied(true);
+
+    setTimeout(() => {
+      setTokenCopied(false);
+    }, 1000);
+  };
+
+  const handleLogout = () => {
+    Cookies.remove("authToken");
+    navigate("/");
+  };
 
   return (
     <div className="passerelle">
       {agentData ? (
         <div className="menu">
           <div className="profile">
-            <h1>{symbol}</h1>
-
-            <span>ID: {agentData.accountId}</span>
+            <h1>{agentData.symbol}</h1>
             <span>Headquarters: {agentData.headquarters}</span>
-
             <span>{agentData.credits} credits</span>
-            <span>Ships : {agentData.shipCount}</span>
+            <span>Ships: {agentData.shipCount}</span>
+            <button className="button" onClick={handleCopyToken}>
+              Token
+            </button>
+            {isTokenCopied && <p>Copied!</p>}
+            <button className="button" onClick={handleLogout}>
+              Logout
+            </button>
           </div>
         </div>
       ) : (
